0d0461d46aae7fdd1fd1c46b9a0a9c7a77d31c7b
bekaDessalegn <bekadessalegn@gmail.com>
2021-08-27 09:54:55 -0400

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - displays all lower and upper case alphabets in ascending order
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
