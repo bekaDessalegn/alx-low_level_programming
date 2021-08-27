5747a922e6e3012af2fa78ad95c2c8c4d3ef7b4e
bekaDessalegn <bekadessalegn@gmail.com>
2021-08-27 10:46:05 -0400

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - displays all numbers of base 16 in lower case.
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+	putchar(ch);
+	putchar(44);
+	putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
