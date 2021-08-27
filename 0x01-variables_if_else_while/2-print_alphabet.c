deb41f86c12d279eb5e835212ad6f3e6508edc85
bekaDessalegn <bekadessalegn@gmail.com>
2021-08-27 09:44:05 -0400

    Lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - displays all lower case alphabets in ascending order
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	return (0);
+}
