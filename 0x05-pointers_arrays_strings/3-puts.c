4da4db371bddf45ac51d0d490a629ed977fe1d29
bekaDessalegn <bekadessalegn@gmail.com>
2021-09-06 14:06:48 -0400

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* _puts - takes a string pointer as a parameter and adds a newline at the end
+* @str: this is the string pointer
+*/
+void _puts(char *str)
+{
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov\n";
+}
