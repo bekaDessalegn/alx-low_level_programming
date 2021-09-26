64def5348e3da882e327005716735852f03d6e0b
bekaDessalegn <bekadessalegn@gmail.com>
2021-09-26 12:47:36 -0400

    my commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,38 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
-  * string_nconcat - ...
-  * @s1: ...
-  * @s2: ...
-  * @n: ...
-  *
-  * Return: ...
-  */
+ * string_nconcat - concatenates two strings
+ * @s1: destination string
+ * @s2: source string
+ * @n: number of bytes from s2 to be copied
+ * Return: concatenated string
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
+	char *p;
+	unsigned int t1, t2, i;
 
 	if (s1 == NULL)
-		s1 = "";
+		t1 = 0;
+	for (t1 = 0; s1[t1]; ++t1)
+		;
 	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[i])
-		i++;
-
-	while (s2[k])
-		k++;
-
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
+		t2 = 0;
+	for (t2 = 0; s2[t2]; ++t2)
+		;
+	if (t2 > n)
+		t2 = n;
+	p = malloc((t1 + t2 + 1) * sizeof(char));
+	if (p == NULL)
 		return (NULL);
-
-	k = 0;
-	while (j < l)
-	{
-		if (j <= i)
-			str[j] = s1[j];
-
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
-	}
-	str[j] = '\0';
-	return (str);
+	for (i = 0; i < t1; i++)
+		p[i] = s1[i];
+	for (i = 0; i < t2; i++)
+		p[t1 + i] = s2[i];
+	p[t1 + t2] = '\0';
+	return (p);
 }
+
